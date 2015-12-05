714a58cfca5678d9dcd0e4d9ba75ad0ecd96330e
Harry Ng <harryworld@gmail.com>
2015-12-06 00:16:14 +0800

    Add Undo using KVC

--- RaiseMan/Employee.swift
+++ RaiseMan/Employee.swift
@@ -11,4 +11,14 @@ import Foundation
 class Employee: NSObject {
     var name: String? = "New Employee"
     var raise: Float = 0.05
+    
+    func validateRaise(raiseNumberPointer: AutoreleasingUnsafeMutablePointer<NSNumber?>) throws {
+        let raiseNumber = raiseNumberPointer.memory
+        if raiseNumber == nil {
+            let domain = "UserInputValidationErrorDomain"
+            let code = 0
+            let userInfo = [NSLocalizedDescriptionKey : "An employee's raise must be a number."]
+            throw NSError(domain: domain, code: code, userInfo: userInfo)
+        }
+    }
 }
--- RaiseMan/ViewController.swift
+++ RaiseMan/ViewController.swift
@@ -23,6 +23,30 @@ class ViewController: NSViewController, NSTableViewDataSource {
         // Update the view, if already loaded.
         }
     }
+    
+    // MARK: - Accessors
+    
+    func insertObject(employee: Employee, inEmployeesAtIndex index: Int) {
+        print("adding \(employee) to the employees array")
+        let undo: NSUndoManager = undoManager!
+        undo.prepareWithInvocationTarget(self).removeObjectFromEmployeesAtIndex(employees.count)
+        if !undo.undoing {
+            undo.setActionName("Add Person")
+        }
+        employees.append(employee)
+    }
+    
+    func removeObjectFromEmployeesAtIndex(index: Int) {
+        let employee: Employee = employees[index]
+        print("removing \(employee) from the employees array")
+        
+        let undo: NSUndoManager = undoManager!
+        undo.prepareWithInvocationTarget(self).insertObject(employee, inEmployeesAtIndex: index)
+        if !undo.undoing {
+            undo.setActionName("Remove Person")
+        }
+        employees.removeAtIndex(index)
+    }
 
     func tableView(tableView: NSTableView, sortDescriptorsDidChange oldDescriptors: [NSSortDescriptor]) {
         let sortDescriptors = tableView.sortDescriptors
