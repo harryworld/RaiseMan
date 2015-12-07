dcaf5f670659554f37fd491f0f324ff88f92eaf5
Harry Ng <harryworld@gmail.com>
2015-12-07 11:42:45 +0800

    Being Editing on Insert

--- RaiseMan/ViewController.swift
+++ RaiseMan/ViewController.swift
@@ -8,20 +8,66 @@
 
 import Cocoa
 
-class ViewController: NSViewController, NSTableViewDataSource {
+class ViewController: NSViewController, NSTableViewDataSource, NSWindowDelegate {
 
-    var employees: [Employee] = []
+    private var KVOContext: Int = 0
+    var employees: [Employee] = [] {
+        willSet {
+            for employee in employees {
+                stopObservingEmployee(employee)
+            }
+        }
+        didSet {
+            for employee in employees {
+                startObservingEmployee(employee)
+            }
+        }
+    }
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var arrayController: NSArrayController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear() {
+        view.window?.delegate = self
+    }
 
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+    
+    @IBAction func addEmployee(sender: NSButton) {
+        guard view.window != nil else { return }
+        
+        let window = view.window!
+        let endedEditing = window.makeFirstResponder(window)
+        if !endedEditing {
+            print("Unable to end editing")
+            return
+        }
+        
+        let undo: NSUndoManager = undoManager!
+        
+        if undo.groupingLevel > 0 {
+            undo.endUndoGrouping()
+            undo.beginUndoGrouping()
+        }
+        
+        let employee = arrayController.newObject() as! Employee
+        arrayController.addObject(employee)
+        arrayController.rearrangeObjects()
+        
+        let sortedEmployees = arrayController.arrangedObjects as! [Employee]
+        let row = sortedEmployees.indexOf(employee)!
+        
+        print("starting edit of \(employee) in row \(row)")
+        tableView.editColumn(0, row: row, withEvent: nil, select: true)
     }
     
     // MARK: - Accessors
@@ -47,10 +93,48 @@ class ViewController: NSViewController, NSTableViewDataSource {
         }
         employees.removeAtIndex(index)
     }
+    
+    // MARK: - Observers
+    
+    func startObservingEmployee(employee: Employee) {
+        employee.addObserver(self, forKeyPath: "name", options: .Old, context: &KVOContext)
+        employee.addObserver(self, forKeyPath: "raise", options: .Old, context: &KVOContext)
+    }
+    
+    func stopObservingEmployee(employee: Employee) {
+        employee.removeObserver(self, forKeyPath: "name", context: &KVOContext)
+        employee.removeObserver(self, forKeyPath: "raise", context: &KVOContext)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if context != &KVOContext {
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+            return
+        }
+        
+        if let keyPath = keyPath, object = object, change = change {
+            var oldValue: AnyObject? = change[NSKeyValueChangeOldKey]
+            if oldValue is NSNull {
+                oldValue = nil
+            }
+            
+            let undo: NSUndoManager = undoManager!
+            print("oldValue=\(oldValue)")
+            undo.prepareWithInvocationTarget(object).setValue(oldValue, forKeyPath: keyPath)
+        }
+    }
 
+    // MARK: - DataSource
+    
     func tableView(tableView: NSTableView, sortDescriptorsDidChange oldDescriptors: [NSSortDescriptor]) {
         let sortDescriptors = tableView.sortDescriptors
         print("sort \(sortDescriptors)")
+    }
+    
+    // MARK: - Delegate
+    
+    func windowWillClose(notification: NSNotification) {
+        employees = []
     }
 }
 
